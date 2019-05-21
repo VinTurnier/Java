9d861c074707460a540feada1fe59c3e003edf1c
VinTurnier <40143479+VinTurnier@users.noreply.github.com>
2019-05-20 21:27:55 -0400

    Create Sort.java

---
+++ sorting/Sort.java
@@ -0,0 +1,27 @@
+public class Sort 
+{
+  public void insertionSort(){
+  
+  }
+  
+  public void selectionSort(){
+  
+  }
+  
+  public void mergeSort(){
+  
+  }
+  
+  public void bubbleSort(){
+  
+  }
+  
+  public void heapSort(){
+  
+  }
+  public void quickSort(){
+  
+  }
+
+
+}
